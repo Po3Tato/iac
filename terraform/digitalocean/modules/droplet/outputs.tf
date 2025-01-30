24aaedc695b08aaec3d0b98fe940762188ba05b7
po3tato <31327412+Po3Tato@users.noreply.github.com>
2025-01-30 17:37:53 -0600

    Update outputs.tf

--- terraform/digitalocean/modules/droplet/outputs.tf
+++ terraform/digitalocean/modules/droplet/outputs.tf
@@ -1,3 +1,8 @@
+output "droplet_ids" {
+  value       = digitalocean_droplet.base_vm[*].id
+  description = "The IDs of the created droplets"
+}
+
 output "public_ips" {
   value       = digitalocean_droplet.base_vm[*].ipv4_address
   description = "Public IP addresses of the droplets"
